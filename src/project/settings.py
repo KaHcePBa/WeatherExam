1e581d8aa1658c960c7e5945e7f158f5b1a71fbf
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-06-12 23:33:57 +0300

    1.4

--- src/project/settings.py
+++ src/project/settings.py
@@ -5,7 +5,6 @@ PROJECT_DIR = Path(__file__).parent.resolve()  # /project
 BASE_DIR = PROJECT_DIR.parent.resolve()  # /src
 REPO_DIR = BASE_DIR.parent.resolve()
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -15,8 +14,11 @@ SECRET_KEY = 'm*gszjafxfu0sy@$o4r53xeg1n4o)s+x%w8xuty_(qs$^$c$72'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    '.weatherexam.herokuapp.com',
+    '127.0.0.1',
+    'localhost',
+]
 
 # Application definition
 
@@ -73,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -83,7 +84,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -103,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -116,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
