37c03901bb8037f11ee5adb589f5258001f233a1
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-06-14 23:07:26 +0300

    1.5 Hello

--- src/applications/index/views.py
+++ src/applications/index/views.py
@@ -1,7 +1,33 @@
-import random
-
 from django.views.generic import TemplateView
+from django.shortcuts import render
+import json
+import urllib.request
 
 
 class IndexView(TemplateView):
     template_name = "index/index.html"
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['minsk']
+
+        source = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q ='
+            + city + '&appid = 32d9a26cf6c27dd72c75db62ea187822').read()
+
+        # converting JSON data to a dictionary
+        list_of_data = json.loads(source)
+
+        # data for variable list_of_data
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+                          + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + 'k',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+        }
+        print(data)
+    else:
+        data = {}
+    return render(request, "main/index.html", data)
